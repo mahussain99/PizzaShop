b1fcc7a3f15e4c6283e8020ee800db2d5f7df15d
Family <mh5212357@gmail.com>
2025-11-09 14:59:03 -0500

    I set Drinks class and continue I am working with this class

--- src/main/java/com/pluralsight/com/shop/drinks/Drink.java
+++ src/main/java/com/pluralsight/com/shop/drinks/Drink.java
@@ -1,4 +1,29 @@
 package com.pluralsight.com.shop.drinks;
 
 public class Drink {
-}
+    private DrinkSize size;
+    private String flavor;
+
+    public Drink(DrinkSize size, String flavor) {
+        this.size = size;
+        this.flavor = flavor;
+    }
+
+    public DrinkSize getSize() {
+        return size;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public double getPrice() {
+
+
+        return 0;
+    }
+
+    }
+
+
+
