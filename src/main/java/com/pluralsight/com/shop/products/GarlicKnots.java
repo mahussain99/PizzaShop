0abe136cce08f3034e671efda13290a6d1ffd794
Family <mh5212357@gmail.com>
2025-11-09 15:46:22 -0500

    I set GralicKnots class and continue I am working with this class

--- src/main/java/com/pluralsight/com/shop/products/GarlicKnots.java
+++ src/main/java/com/pluralsight/com/shop/products/GarlicKnots.java
@@ -1,4 +1,24 @@
 package com.pluralsight.com.shop.products;
 
 public class GarlicKnots {
+    private double amount ;
+
+    public GarlicKnots(double amount) {
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+    // GarlicNots price $1.50
+     public double getPrice(){
+        return  amount * 1.50;
+
+     }
+
+
 }
