7e402bf069527f6d5ad980c0e1ac0add68b7acfc
Family <mh5212357@gmail.com>
2025-11-09 22:46:51 -0500

    I set Topping  class and continue I am working with this class

--- src/main/java/com/pluralsight/com/shop/products/Topping.java
+++ src/main/java/com/pluralsight/com/shop/products/Topping.java
@@ -1,4 +1,27 @@
 package com.pluralsight.com.shop.products;
 
-public class Topping {
+public abstract class Topping {
+    private String name;
+
+    public Topping(String name, String s) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addExtra(Topping Tp) {
+
+    }
+
+    public double getCost() {
+
+
+        return 0;
+    }
 }
