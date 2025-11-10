3ab4656f607bf9aa5bdb882c05ed8a3da9040015
Family <mh5212357@gmail.com>
2025-11-10 11:59:23 -0500

    I update Sauces and Side  class and continue I am working with this class

--- src/main/java/com/pluralsight/com/shop/regularTopping/Sauces.java
+++ src/main/java/com/pluralsight/com/shop/regularTopping/Sauces.java
@@ -1,4 +1,20 @@
 package com.pluralsight.com.shop.regularTopping;
 
-public class Sauces {
+import com.pluralsight.com.shop.allTopping.Topping;
+import com.pluralsight.com.shop.products.Pizza;
+
+public class Sauces extends Topping {
+    private String name;
+
+    public Sauces(String name) {
+        super(name);
+    }
+
+    @Override
+    public double getPrice(Pizza.PizzaSize size) {
+        return 0;
+    }
 }
+
+
+
--- src/main/java/com/pluralsight/com/shop/regularTopping/Side.java
+++ src/main/java/com/pluralsight/com/shop/regularTopping/Side.java
@@ -1,4 +1,20 @@
 package com.pluralsight.com.shop.regularTopping;
 
+import com.pluralsight.com.shop.products.Pizza;
+
 public class Side {
+    private String name;
+
+    public Side(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public double getPrice(){
+        return 0.00;
+    }
+
+
 }
